41cf72b0589b08a401a12fbcf536f938d14bc20a
komalkashyap <komalkashyapmajri403@gmail.com>
2021-12-02 23:36:24 +0530

    [KOMAL KASHYAP] Added even or odd program

---
+++ EvenOrOdd/EvenOrOdd/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Enter a Number");
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("Entered Number is an even number");
+                Console.Read();
+
+            }
+            else
+            {
+                Console.Write("Entered Nuumber is an odd number");
+                Console.Read();
+            }
+        }
+    }
+}
